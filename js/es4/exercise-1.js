432fc17c56080a73351c85ec90d99b0f1ecff5ca
Daniele Canale <danycnc95@gmail.com>
2022-03-07 12:00:25 +0100

    svolto es 4

--- js/es4/exercise-1.js
+++ js/es4/exercise-1.js
@@ -3,15 +3,31 @@
 // cto => 1800
 // developer => 1500
 // default => 1000
-function calculateSalary(role) {
-  // ...
-}
-
 const ceoSalary = calculateSalary('ceo');
 const managerSalary = calculateSalary('manager');
 const ctoSalary = calculateSalary('cto');
 const developerSalary = calculateSalary('developer');
 const otherSalary = calculateSalary('other');
+
+function calculateSalary(role) {
+  // ...
+  if (role==='ceo') {
+    return '2200€';
+  }
+  else if (role==='manager') {
+   return '1800€'
+  }
+  else if (role==='cto') {
+    return '1800€'
+  }
+  else if (role==='developer') {
+    return '1500€'
+  }
+  else {
+    return '1000€'
+  }
+}
+
 
 console.log(ceoSalary);
 console.log(managerSalary);
