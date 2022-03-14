031d7c69bccc80c1fd21c36a5f16f478cbc2c5ca
Daniele Canale <danycnc95@gmail.com>
2022-03-14 15:45:58 +0100

    svolto es25

--- js/es25/exercise-1.js
+++ js/es25/exercise-1.js
@@ -1,17 +1,48 @@
-// const person = {
-//   // ...
-//   }
+const person = {
+  // ...
 
-function person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.fullName = function () {
+  firstName: "",
+  lastName: "",
+
+  get firstName() {
+    return firstName;
+  },
+
+  get lastName() {
+    return lastName;
+  },
+
+  set setfirstName(value) {
+    firstName = value;
+  },
+
+  set setlastName(value) {
+    lastName = value;
+  },
+
+  fullName: function () {
     return `${this.firstName} ${this.lastName}`;
-  };
-}
+  },
+};
 
-let john = new person("John", "Doe");
-let simon = new person("Simon", "Collins");
+// function person(firstName, lastName) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.fullName = function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   };
+// }
 
+// let john = new person("John", "Doe");
+// let simon = new person("Simon", "Collins");
+
+let john = Object.create(person);
+john.setfirstName = "Jonh";
+john.setlastName = "Doe";
 console.log(john.fullName()); // John Doe
+
+let simon = Object.create(person);
+simon.setfirstName = "Simon";
+simon.setlastName = "Collins";
+
 console.log(simon.fullName()); // Simon Collins
