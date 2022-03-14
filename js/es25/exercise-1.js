5884f792deb59786a31e979bbd8db9e8903536b7
Daniele Canale <danycnc95@gmail.com>
2022-03-14 15:46:33 +0100

    svolto es25

--- js/es25/exercise-1.js
+++ js/es25/exercise-1.js
@@ -25,17 +25,6 @@ const person = {
   },
 };
 
-// function person(firstName, lastName) {
-//   this.firstName = firstName;
-//   this.lastName = lastName;
-//   this.fullName = function () {
-//     return `${this.firstName} ${this.lastName}`;
-//   };
-// }
-
-// let john = new person("John", "Doe");
-// let simon = new person("Simon", "Collins");
-
 let john = Object.create(person);
 john.setfirstName = "Jonh";
 john.setlastName = "Doe";
