cfe866dcdf2dd3ee307056266ee46ec8a2f69af5
Daniele Canale <danycnc95@gmail.com>
2022-03-14 11:48:12 +0100

    svolto es11

---
+++ js/es11/exercise-1.js
@@ -0,0 +1,17 @@
+// const sum = function (a, b) {
+//   return a + b;
+// }
+
+// const log = function (value) {
+//   console.log(value);
+// }
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+const log = (value) => {
+  console.log(value);
+};
+
+log(sum(2, 5));
