bab692c690afb3b9512d4874a0d253ed5e974314
Daniele Canale <danycnc95@gmail.com>
2022-03-16 10:41:38 +0100

    svolto es14

---
+++ js/es14/exercise-1.js
@@ -0,0 +1,23 @@
+function greet(greeting) {
+  // ...
+
+  function greetWithName(name) {
+    return `${greeting}, ${name}`;
+  }
+  console.log(greeting);
+  return greetWithName;
+}
+
+const greetWithHello = greet("Hello");
+const greetWithBye = greet("Bye");
+const greetWithHi = greet("Hi");
+
+const greetPaulWithHello = greetWithHello("Paul");
+const greetPaulWithHi = greetWithHi("Paul");
+const greetJohnWithBye = greetWithBye("John");
+const greetSimonWithHello = greetWithHello("Simon");
+
+console.log(greetPaulWithHello);
+console.log(greetPaulWithHi);
+console.log(greetJohnWithBye);
+console.log(greetSimonWithHello);
