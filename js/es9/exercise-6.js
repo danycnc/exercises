7f5e554cbebaedc123b38d61546c10255687b1f2
Daniele Canale <danycnc95@gmail.com>
2022-03-16 10:27:52 +0100

    corretto es9

--- js/es9/exercise-6.js
+++ js/es9/exercise-6.js
@@ -1,14 +1,22 @@
 function getKeys(obj) {
   // ...
-  return Object.keys(obj);
+  // return Object.keys(obj);
+
+  let keysArray = [];
+
+  for (let key in obj) {
+    keysArray.push(key);
+  }
+
+  return keysArray;
 }
 
 const person = {
-  firstName: 'John',
-  lastName: 'Doe',
+  firstName: "John",
+  lastName: "Doe",
   age: 32,
-  city: 'Rome',
-  job: 'Developer',
+  city: "Rome",
+  job: "Developer",
 };
 
 const keys = getKeys(person);
