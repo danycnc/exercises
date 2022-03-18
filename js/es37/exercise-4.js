eb6d0571b3b7cccd5ee138ddc461f2a8f0ae78d5
Daniele Canale <danycnc95@gmail.com>
2022-03-18 12:35:00 +0100

    svolto es37

---
+++ js/es37/exercise-4.js
@@ -0,0 +1,24 @@
+class BankAccount {
+    constructor (amount){
+        this.amount=amount
+    }
+
+    deposit (value) {
+        this.amount+=value
+    }
+
+    withdraw (value) {
+        this.amount-=value
+    }
+
+    view() {
+        console.log(`Current amount is: ${this.amount}`)
+    }
+}
+
+
+const bankAccount = new BankAccount(1000);
+bankAccount.deposit(500);
+bankAccount.deposit(200);
+bankAccount.withdraw(800);
+bankAccount.view();
